49ad1a7d34e5b0b0e61afe3194dd5daf0be0a3eb
sshankar19 <sshankar19@gmail.com>
2014-09-21 11:12:56 -0400

    main commit

---
+++ src/Main.py
@@ -0,0 +1,22 @@
+#ask the user the event (location, city, zipcode)
+
+print "Welcome to Spoons&Tunes"
+print "Spoons&Tunes helps to organize your event, by:"
+print "    1) Making a Spotify playlist custom for your hangout based on your personal taste and bands in your area."
+print "    2) Finding the perfect food choice for you to order (cater) Food from by using the ordr.in API"
+#print "    3) Tweeting to your friends on twitter and letting them know about your hangout!
+
+location = raw_input("Please enter the address of your event:")
+print location
+city = raw_input("Now the city please:")
+print city
+zipcode = raw_input("Please enter the address of your event:")
+print location
+location = raw_input("Please enter the address of your event:")
+print location
+username = raw_input("Please enter the address of your event:")
+print location
+playlistName = raw_input("Please enter the address of your event:")
+print location
+numberSongs = raw_input("Please enter the address of your event:")
+print location
